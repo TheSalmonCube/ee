Unit fixing: change from 1 on everything to proper particle physics natural units c = hbar = e0 = 1, with particle mass and charge using experimental values of fine structure constant (dimensionless) and electron mass in MeV. Bohr radius used for reference grid spacing.

hamiltonian.py
@@ -4,7 +4,10 @@ from scipy import sparse
 
 # H = h/2m ∂x^2 + V
 
-# natural units are used, h bar is 1
+# particle physics natural units are used: c = hbar = e0 = 1
+# from fine structure constant alpha = 1/137.036
+# elementary charge = sqrt(4 pi alpha) = 0.30282
+# mass of electron = 0.51099895069 MeV 
 
 # 3 dimensions
 
@@ -42,7 +45,7 @@ def build_hamiltonian(shape: tuple, V: np.ndarray, m: float, dx: float):
         idx = index(shape, point)
         row.append(idx)
         col.append(idx)
-        obj.append(-0.5 / m * (-2 * dimensions) + V[idx])
+        obj.append(-0.5 / (m * dx**2) * (-2 * dimensions) + V[idx])
 
         # OFF DIAGONAL
         # Kinetic: 1/2m * (1) / dx^2 for each dimension
@@ -64,12 +67,15 @@ def build_hamiltonian(shape: tuple, V: np.ndarray, m: float, dx: float):
     
 ### DIFFERENT POTENTIALS
 
-def coulomb_potential(shape: tuple, charge: float, nucleus_pos: tuple):
+def coulomb_potential(shape: tuple, charge1: float, charge2: float, nucleus_pos: tuple, dx: float):
     V = np.zeros(np.prod(shape))
+    reciprocalk = 0.25 / np.pi # 1/4pi (epsilon0 is 1 in natural units)
+    charge = charge1 * charge2 * reciprocalk
 
     for point in np.ndindex(shape):
         idx = index(shape, point)
-        V[idx] = -charge / np.linalg.norm(np.array(point) - np.array(nucleus_pos))
+        r = np.linalg.norm(np.array(point) - np.array(nucleus_pos))
+        V[idx] = -charge / r
 
     return V
 

search.py
@@ -6,6 +6,15 @@ import algorithms
 import hamiltonian
 import visualize
 
+# particle physics natural units are used: c = hbar = e0 = 1
+# from fine structure constant alpha = 1/137.036
+# elementary charge = sqrt(4 pi alpha) = 0.30282
+# mass of electron = 0.51099895069 MeV
+
+ELECTRON_MASS = 0.51099895069 # MeV
+ELECTRON_CHARGE = 0.30282 # dimensionless
+GRID_SPACE = 268 # Mev-1. The bohr radius, the peak of 1s, should be at 268 Mev-1 or 1/(Me alpha).
+
 def search_v1(H: sparse.csr_matrix, m: int, k_list: list):
     n = H.shape[0]
     v0 = np.random.rand(n)
@@ -25,11 +34,14 @@ if __name__ == "__main__":
     K = [0, 1, 2, 3, 4]
     M = 100
 
-    V = hamiltonian.coulomb_potential(shape, 1, (25.5, 25.5, 25.5))
-    H = hamiltonian.build_hamiltonian(shape, V, 1, 1)
+    V = hamiltonian.coulomb_potential(shape, ELECTRON_CHARGE, ELECTRON_CHARGE, (25.5, 25.5, 25.5), GRID_SPACE)
+    H = hamiltonian.build_hamiltonian(shape, V, ELECTRON_MASS, GRID_SPACE)
     print(H.nnz)
 
     eigenvalues, eigenvectors = search_v1(H, m=M, k_list=K)
+
+    plt.imshow(V.reshape(shape)[:, :, 25])
+    plt.show()
 
     for k, eigenvector in enumerate(eigenvectors):
         np.save(f'wavefunctions/coulomb_k{k}_m{M}_{shape}.npy', eigenvector)